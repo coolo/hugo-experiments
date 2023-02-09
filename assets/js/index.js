a4f3a38c1dc54af9220953ec8ca16060896b0ff4
Stephan Kulow <stephan@kulow.org>
2023-02-09 21:01:54 +0100

    Tune the breakpoints a little

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,20 @@
 // Import the Bootstrap components we want to use.
 // See https://github.com/twbs/bootstrap/blob/main/js/index.umd.js
-import Button from 'js/bootstrap/src/button.js'
 import Carousel from 'js/bootstrap/src/carousel.js'
 import Collapse from 'js/bootstrap/src/collapse.js'
-import Dropdown from 'js/bootstrap/src/dropdown.js'
-import Offcanvas from 'js/bootstrap/src/offcanvas.js'
-import Popover from 'js/bootstrap/src/popover.js'
 import ScrollSpy from 'js/bootstrap/src/scrollspy.js'
-import Tab from 'js/bootstrap/src/tab.js'
-import Toast from 'js/bootstrap/src/toast.js'
-import Tooltip from 'js/bootstrap/src/tooltip.js'
 
 export default {
-  Button,
   Carousel,
   Collapse,
-  Dropdown,
-  Offcanvas,
-  Popover,
   ScrollSpy,
-  Tab,
-  Toast,
-  Tooltip
 }
 
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
 
-  const masonryGrid = document.querySelector(".masonry-grid");
+    const masonryGrid = document.querySelector(".masonry-grid");
     if (masonryGrid) {
         var msnry = new Masonry(masonryGrid, {
             itemSelector: ".masonry-grid-item",
